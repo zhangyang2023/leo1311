77b6407ff2ab234b3577775e54a296b6d06e3c41
zhangyang2023 <294246238@qq.com>
2022-04-27 17:10:49 +0800

    none

---
+++ consumer_local.py
@@ -0,0 +1,20 @@
+
+import json
+from kafka import KafkaConsumer
+
+CONSUMER = KafkaConsumer('kpi-1c9e9efe6847bc4723abd3640527cbe9', bootstrap_servers= ['localhost:9092'])
+
+def main():
+	print('test consumer')
+	i = 0
+	for message in CONSUMER:
+		i += 1
+		data = json.loads(message.value.decode('utf8'))
+		print(data)
+
+
+if __name__ == '__main__':
+	'''
+		start to consume kafka
+	'''
+	main()
